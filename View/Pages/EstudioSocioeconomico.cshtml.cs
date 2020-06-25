3806d803d8dceea1b6ed39633419c70a6848e187
hhhh <najeracastrojuanpablo.059.yuc@gmail.com>
2020-06-25 09:21:53 -0500

    Dev EconomicStudy 1.0

--- View/Pages/EstudioSocioeconomico.cshtml.cs
+++ View/Pages/EstudioSocioeconomico.cshtml.cs
@@ -4,16 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Model;
+using Control;
+
 
 namespace Proyecto
 {
     public class EstudioSocioeconomicoModel : PageModel
     {
+        private readonly IRepository<Visit> repository;
+
         [BindProperty]
-         public int Codigo { get; set; }
-         
+         public Visit visit { get; set; }
+         public string Message { get; set; }
+
+         public EstudioSocioeconomicoModel(IRepository<Visit> repository)
+         {
+             this.repository = repository;
+             visit = new Visit();
+         }
         public void OnGet()
         {
+
+        }
+
+        public void OnPost(){
+            visit = repository.Get(visit.Id) ?? new Visit();
+            if(visit.Id <= 0)
+                Message = "No se encontro ningun id con ese codigo, intentelo nuevamente";
         }
     }
 }
